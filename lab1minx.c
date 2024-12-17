48949d1523ebe327c9fe20c83dabc317d0e2c4bc
s6704062610011 <s6704062610011@email.kmutnb.ac.th>
2024-12-17 11:28:56 +0700

    ข้อ 1

---
+++ lab1minx.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=10;
+    int b=15;
+    if (b>a)
+    {
+        printf("%d greater than %d",b,a);
+    }
+    else
+    {
+        printf("%d greater than %d",a,b);
+    }
+    return 0;
+}
