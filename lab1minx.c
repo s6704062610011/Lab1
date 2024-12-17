46553dcea6f7da8e9437946e38ffc895c6cb2f35
s6704062610011 <s6704062610011@email.kmutnb.ac.th>
2024-12-17 11:31:01 +0700

    ข้อ 2

--- lab1minx.c
+++ lab1minx.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int a=10;
-    int b=15;
-    if (b>a)
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    if (b > a)
     {
-        printf("%d greater than %d",b,a);
+        printf("%d greater than %d\n", b, a);
+        printf("greater than %d",(b-a));
     }
     else
     {
-        printf("%d greater than %d",a,b);
+        printf("%d greater than %d\n", a, b);
+        printf("greater than %d",(a-b));
     }
     return 0;
 }
